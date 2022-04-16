3ca84c3516f982cc64e5ae097cf8b1006f20b143
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-04-16 09:37:20 +0530

    Create testReceiver.cpp

---
+++ testReceiver.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "BMSDataReceiver.h"
+
+TEST_CASE("Verify Read parameters from file - Valid Case") {
+  ReadDataFromConsole();
+}
