6630f96597970405f70e6a4b7a6d2f2b7bc49414
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-04-17 23:28:26 +0530

    Update BMSDataReceiver.c

--- BMSDataReceiver.c
+++ BMSDataReceiver.c
@@ -2,7 +2,25 @@
 #include <stdlib.h>
 #include "BMSDataReceiver.h"
 
-void ReadDataFromConsole(void)
+void ReadDataFromConsole(int current[], int temperature[])
+{
+    char unusedData[50];
+    for(int i=0; i<NoOfSamples; i++)
+    {
+        scanf("%50s",unusedData);
+        scanf("%d",&current[i]);
+        scanf("%50s",unusedData);
+        scanf("%50s",unusedData);
+        scanf("%d",&temperature[i]);
+    }
+}
+
+void receiverSideHandling(int current[], int temperature[])
 {
   printf("Running Receiver\n");
+  ReadDataFromConsole(current,temperature);
+   for(int i=0; i<NoOfSamples; i++)
+   {
+      printf("Current: %d, Temperature: %d\n",current[i],temperature[i]);
+   }
 }
