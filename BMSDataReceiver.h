5cb4ec51f54083a9589a6a7f3d500d6fb082a84c
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-04-17 23:59:03 +0530

    Update BMSDataReceiver.h

--- BMSDataReceiver.h
+++ BMSDataReceiver.h
@@ -2,3 +2,6 @@
 
 void receiverSideHandling(int current[], int temperature[]);
 void ReadDataFromConsole(int current[], int temperature[]);
+int findMaximum(int data[]);
+int findMinimum(int data[]);
+float findAverage(int data[]);
