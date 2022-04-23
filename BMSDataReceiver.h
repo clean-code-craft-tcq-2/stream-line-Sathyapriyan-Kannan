dc1c61039cd34c08cf949771169a9d4659ae97aa
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-04-23 12:48:56 +0530

    Update BMSDataReceiver.h

--- BMSDataReceiver.h
+++ BMSDataReceiver.h
@@ -1,9 +1,10 @@
 #define NoOfSamples 50
 #define WindowLength 5
 
-void receiverSideHandling(int current[], int temperature[], void (*fn_ptrPrintOutput)(char message[]));
+void receiverSideHandling(int current[], int temperature[], void (*fn_ptrPrintOutput)(char message[]),
+                          void (*fn_ptrReadData)(int current[], int temperature[], int index));
 void ReadDataFromConsole(int current[], int temperature[], int index);
 int findMaximum(int data[], int size);
 int findMinimum(int data[], int size);
-float findAverage(int data[], int size);
+float calcMovingAverage(int data[], int size);
 void printOnConsole(char message[]);
